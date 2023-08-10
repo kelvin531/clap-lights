925df6948e8c011e28fb8b95a490c0622e070d10
kelvin531 <141890949+kelvin531@users.noreply.github.com>
2023-08-10 11:59:38 +0800

    Update pxt.json, main.blocks, main.ts, pxt.json

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ let lightsOn = 0
 basic.forever(function () {
     lightsOn = 1
     basic.showNumber(input.soundLevel())
-    if (input.soundLevel() >= 90) {
+    datalogger.log(datalogger.createCV("dB", input.soundLevel()))
+    serial.writeValue("LV", input.soundLevel())
+    if (input.soundLevel() >= 80) {
         basic.showLeds(`
             # # # . .
             # . . # .
